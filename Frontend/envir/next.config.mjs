743bc75f25b745a6e3d9c8edd0f88a6a87f392a3
SuperexMack <unilexerox@gmail.com>
2025-01-27 17:24:37 +0530

    Added some effects

--- Frontend/envir/next.config.mjs
+++ Frontend/envir/next.config.mjs
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: '/(.*)', // Apply these headers to all routes
-          headers: [
-            {
-              key: 'Cross-Origin-Opener-Policy',
-              value: 'same-origin',
-            },
-            {
-              key: 'Cross-Origin-Embedder-Policy',
-              value: 'require-corp',
-            },
-            {
-              key: 'Access-Control-Allow-Origin',
-              value: '*', // Adjust this to your frontend domain for more security
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
