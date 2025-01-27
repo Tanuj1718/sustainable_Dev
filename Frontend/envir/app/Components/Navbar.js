1d8091dc18914ed48ba355f50ae2c904b65a849d
SuperexMack <unilexerox@gmail.com>
2025-01-27 15:24:28 +0530

    Some changes

--- Frontend/envir/app/Components/Navbar.js
+++ Frontend/envir/app/Components/Navbar.js
@@ -8,7 +8,7 @@ export function Navbar(){
               <h1 className="font-bold text-[30px] text-green-600">Sustainable Room</h1>
             </div>
             <div className="flex space-x-12 absolute list-none right-[20%]">
-                <li className="text-[20px] font-bold text-slate-500 hover:cursor-pointer hover:border-b-2 border-green-700">Home</li>
+                <Link href={"/"}><li className="text-[20px] font-bold text-slate-500 hover:cursor-pointer hover:border-b-2 border-green-700">Home</li></Link>
                 <li className="text-[20px] font-bold text-slate-500 hover:cursor-pointer hover:border-b-2 border-green-700">About</li>
                <Link href={"/UserPage"}><li className="text-[20px] font-bold text-slate-500 hover:cursor-pointer hover:border-b-2 border-green-700">DashBoard</li></Link> 
                 <li className="text-[20px] font-bold text-slate-500 hover:cursor-pointer hover:border-b-2 border-green-700">Donate Us</li>
